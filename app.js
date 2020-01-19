9d81d2cc27c3911c0aa178556799923c870248a7
Drew <drewradley@gmail.com>
2020-01-19 14:53:27 -0800

    OOMPHDB ver1

--- app.js
+++ app.js
@@ -14,7 +14,11 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://drewradley:Robert1313@cluster0-xfvkm.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://drewradley:Robert1313@cluster0-xfvkm.mongodb.net/local_library?retryWrites=true&w=majority';
+
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
